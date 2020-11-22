9ad60121e1a55313e7d599493652bc5a395e5968
merajrahat <merajrahat@yahoo.com>
2020-11-22 08:43:29 -0500

    update

--- ebay/src/main/java/com/ebay/HomePage.java
+++ ebay/src/main/java/com/ebay/HomePage.java
@@ -5,12 +5,45 @@ import org.openqa.selenium.support.FindBy;
 
 public class HomePage {
 
+
+
     @FindBy(id="gh-ac")
     private WebElement searchBar;
 
+    @FindBy(id = "gh-btn")
+    private WebElement searchBtn;
+
+    @FindBy(linkText = "Sign in")
+    private WebElement signInBtn;
+
+    @FindBy(linkText = "Daily Deals")
+    private WebElement dailyDealsBtn;
+
+    @FindBy(id = "gh-shop-a")
+    private WebElement shopByCategory;
+
+    @FindBy(linkText = "Collectibles")
+    private WebElement collectibles;
 
     public void typeOnSearchBar() {
 
         searchBar.sendKeys("Java Books");
     }
+    public void clickOnSearchButton(){
+
+        searchBtn.click();
+    }
+    public void clickOnSignInButton(){
+
+        signInBtn.click();
+    }
+    public void clickOnDaileyDeals(){
+
+        dailyDealsBtn.click();
+    }
+    public void clickOnShopByCategory(){
+        shopByCategory.click();
+        collectibles.click();
+    }
+
 }
--- ebay/src/test/java/com/ebaytest/HomePageValidations.java
+++ ebay/src/test/java/com/ebaytest/HomePageValidations.java
@@ -2,15 +2,41 @@ package com.ebaytest;
 
 import com.ebay.HomePage;
 import com.peoplentech.webautomation.base.TestBase;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 public class HomePageValidations extends TestBase {
 
-    @Test
+    @Test (enabled = false)
     public void validateUserBeingAbleToTypeOnSearchBar(){
 
         HomePage homePage = PageFactory.initElements(driver,HomePage.class);
         homePage.typeOnSearchBar();
+    }
+
+    @Test (enabled = false)
+    public void validateUserBeingAbleToSearchForAnItem(){
+        HomePage homePage = PageFactory.initElements(driver,HomePage.class);
+        homePage.typeOnSearchBar();
+        homePage.clickOnSearchButton();
+    }
+
+    @Test (enabled = false)
+    public void validateUserBeingAbleToClickOnSignInButton() {
+        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+        homePage.clickOnSignInButton();
+    }
+
+    @Test (enabled = false)
+    public void validateUserBeingAbleToClickOnDailyDealsButton(){
+        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+        homePage.clickOnDaileyDeals();
+    }
+
+    @Test
+    public void validateUserBeingAbleToClickOnShopByCategoryAndSelectACategory(){
+        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+        homePage.clickOnShopByCategory();
     }
 }
